4d5e5db2c88871a8fd3c6baa60a64a6406b0f2a2
m1jenkins <55204743+m1jenkins@users.noreply.github.com>
2026-06-05 10:54:05 -0500

    Remove reviews component and references

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
 import { Features } from './components/Features';
-import { Reviews } from './components/Reviews';
 import { ContactForm } from './components/ContactForm';
 import { ServiceArea } from './components/ServiceArea';
 import { Footer } from './components/Footer';
@@ -111,7 +110,6 @@ const App: React.FC = () => {
           <main>
             <Hero />
             <Features />
-            <Reviews />
             <ContactForm prefilledDetails={bookingDetails} />
             <ServiceArea onNavigate={handleNavigate} />
           </main>
